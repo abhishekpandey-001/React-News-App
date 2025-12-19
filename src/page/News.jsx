076db54f97162b1a1c19ceb38c452bcb569bc81a
Abhishek Pandey <abhishekpandey.codes@gmail.com>
2025-12-19 17:15:36 +0530

    finished navbar

---
+++ src/page/News.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Wrapper from '../components/Wrapper'
+
+const News = () => {
+    return (
+        <Wrapper>
+            <div>
+                News
+            </div>
+        </Wrapper>
+    )
+}
+
+export default News
